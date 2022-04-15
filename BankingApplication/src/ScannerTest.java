8b008f021a5c1b689761ee51945a641c8fda68b7
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-15 15:18:05 -0500

    update to JAVA Bank App

--- BankingApplication/src/ScannerTest.java
+++ BankingApplication/src/ScannerTest.java
@@ -1,13 +1,36 @@
-import java.util.Scanner;          //must be imported for scanner to work//package, util, scanner
+import java.util.Scanner;
 // class
+
+//GOALS: Application displays welcome message to user with name, then five options
 
 
 public class ScannerTest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  //takes input from the user
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+        //Below is from tutorial for reference:
+//        Scanner scanner = new Scanner(System.in);  //takes input from the user
+//        System.out.println("Enter your character: ");
+//        char c = scanner.next().charAt(0); //specify the index of the string
+//        System.out.println("value = " + c);
+        //Above is from tutorial for reference ^
     }
+
+    class BankAccount {
+        int balance;
+        int previousTransaction;
+        String customerName;
+        String customerID;
+
+        void deposit (int amount) {
+            if (amount != 0) {
+                balance = balance + amount;
+                
+            }
+        }
+    }
+
+
 }
+
+
+
