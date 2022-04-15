a46b7afdbc86d5c319dc20837def2ce1cc99f809
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-15 15:26:45 -0500

    update to JAVA BANK APP

--- BankingApplication/src/ScannerTest.java
+++ BankingApplication/src/ScannerTest.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 // class
 
 //GOALS: Application displays welcome message to user with name, then five options
@@ -21,15 +20,44 @@ public class ScannerTest {
         String customerName;
         String customerID;
 
-        void deposit (int amount) {
-            if (amount != 0) {
-                balance = balance + amount;
-                
+        void deposit(int amount) {        //first method, amount is paramater
+            if (amount != 0) {             // don't perform operation if amount is 0
+                balance = balance + amount;   //increments
+                previousTransaction = amount;  //load previous amount
             }
         }
+
+        void withdrawal(int amount) {
+            if (amount != 0) {                //don't perform operation if amount is 0
+                balance = balance - amount;   //decrements, or deducts amount from balance
+                previousTransaction = amount;
+            }
+        }
+
+        void getPreviousTransaction() {
+            if (previousTransaction > 0) {  //if previous transaction is greater than 0
+                System.out.println("Deposited: " + previousTransaction);
+            } else if (previousTransaction < 0) {
+                System.out.println("Withdrawn: " + Math.abs(previousTransaction));  //absolute
+                // method takes any value and returns the absolute value so it shows withdrawn
+                // 1000 versus withdrawn -1000
+            } else {
+                System.out.println("No recently recorded transactions");
+            }
+        }
+
+        void showMenus() {
+            System.out.println("Enter 1 to deposit");
+            System.out.println("Enter 2 to withdrawal");
+            System.out.println("Enter 3 to check balance");
+            System.out.println("Enter 4 to get previous transaction");
+            System.out.println("Enter 5 to exit");
+        }
+
+        void setCustomerID(String id) {
+            customerID = id;
+        }
     }
-
-
 }
 
 
