f598438e9888622f58937fd26296936c2d2bfb68
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-16 18:30:09 -0500

    Banner mvp complete

--- netflix-optimized/src/Banner.js
+++ netflix-optimized/src/Banner.js
@@ -13,7 +13,7 @@ function Banner() {
             setMovie(
                 request.data.results[
                     Math.floor(Math.random() * request.data.results.length - 1)
-                    ]     //this simplifies to return one item from the array
+                    ]     //this simplifies to return one item from the array, randomly
             );
             return request;
         }
@@ -26,12 +26,26 @@ function Banner() {
 
 
     return (
-        <header>      {/* <<<background image */}
-            {/*    title */}
-            {/*    div > 2 buttons */}
-            {/*    description */}
-        </header>
-    )
-}
+        <header className="banner"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: `url(
+                    "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+                )`,
+                    backgroundPosition: 'center center',
+                }}
+        >
 
+            <div className="banner_contents">
+                <h1>
+                    {movie?.title || movie?.name || movie?.original_name}   
+                </h1>
+                
+            </div>
+
+        </header>
+    );
+}
+// the || takes care of the edge cases where the movie is null
+// the ? for example on the  movie? is new to React and is called optional chaining
 export default Banner;
