d3f0ee9ea66261f7608bcdb27ad9837802ff2bd4
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-17 09:38:06 -0500

    JavaScript daily practice and next problem set up

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -79,7 +79,85 @@
 // }
 
 //Problem:
-//TODO: For 17April2022
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    //old if else statement:
+
+    // if (val === "bob") {
+    //     answer = "Marley";
+    // } else if (val === 42) {
+    //     answer = "The Answer";
+    // } else if (val === 1) {
+    //     answer = "There is no #1";
+    // } else if (val === 99) {
+    //     answer = "Missed me by this much!";
+    // } else if (val === 7) {
+    //     answer = "Ate Nine";
+    // }
+
+    //new switch statement:
+//     switch (val) {
+//         case "bob":
+//             answer = "Marley";
+//             break;
+//         case 42:
+//             answer = "The Answer";
+//             break;
+//         case 1:
+//             answer = "There is no #1";
+//             break;
+//         case 99:
+//             answer = "Missed me by this much!";
+//             break;
+//         case 7:
+//             answer = "Ate Nine";
+//             break;
+//     }
+//
+//     // Only change code above this line
+//     return answer;
+// }
+
+// chainToSwitch(7);
+// console.log(chainToSwitch(7));
+
+// ****************************************************************
+//TODO for April 18 2022
+
+// Returning Boolean Values from Functions
+// You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+//
+//     Sometimes people use an if/else statement to do a comparison, like this:
+//
+//     function isEqual(a, b) {
+//         if (a === b) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+//
+// function isEqual(a, b) {
+//     return a === b;
+// }
+// Fix the function isLess to remove the if/else statements.
+
+// TODO: Problem:
+//     function isLess(a, b) {
+//         // Only change code below this line
+//         if (a < b) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//         // Only change code above this line
+//     }
+//
+//     isLess(10, 15);
+
+
 
 
 
