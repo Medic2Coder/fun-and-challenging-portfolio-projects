b9a1aef7446aacb7247a25b4d060d5142bc134e4
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-19 07:29:26 -0500

    basic set up for Student Database JAVA app

--- StudentDatabaseApp/src/studentdatabaseapp/Student.java
+++ StudentDatabaseApp/src/studentdatabaseapp/Student.java
@@ -1,16 +1,29 @@
 package studentdatabaseapp;
 
-public class Student {
-     private String firstName;
-     private String lastName;
-    private String gradeYear;
-    private int studentID;
+import java.util.Scanner;
 
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
     private String courses;
-     private int tuitionBalance;
-     private int costOfCourse = 600;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
 
     //Constructor: prompt user for student name and year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student's first name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student's last name: ");
+        this.lastName = in.nextLine();
+        System.out.println("1 -  for Freshmen\n2- for Sophomore\n3 - Junior\n4 - Senior\nEnter " +
+                "student class level: ");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " is in grade " + gradeYear);
+        
+    }
 
     //Generate an Id
 
--- StudentDatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentDatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,8 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+        //create a new student here to test main method
+        Student  stu1 = new Student();
         //Ask how many students we want to add
 
 
