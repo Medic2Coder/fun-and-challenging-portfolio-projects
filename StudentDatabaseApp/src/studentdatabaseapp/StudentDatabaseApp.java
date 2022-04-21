868cf61299ea00e7c8d902b62e5794cc71524585
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-21 09:24:04 -0500

    JAVA APP Student Database improvement

--- StudentDatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentDatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,15 +1,38 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+
         //create a new student here to test main method
-        Student  stu1 = new Student();
-        //call the method
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
-        //Ask how many students we want to add
+//        Student  stu1 = new Student();
+//        //call the method
+//        stu1.enroll();
+//        stu1.payTuition();
+//        System.out.println(stu1.toString());
+
+
+        //Ask how many students we want to add to and do it in a way that is more efficient than
+        // hard coding each new student
+        System.out.println("How many students do you want to add?");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+        //iterate through the array and add each student
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            //call the method
+            students[n].enroll();
+            students[n].payTuition();
+//            System.out.println(students[n].toString());
+
+        }
+        System.out.println(students[0].toString());
+        System.out.println(students[1].toString());
+        System.out.println(students[2].toString());
+
 
 
     }
