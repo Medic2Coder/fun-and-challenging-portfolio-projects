76e3065cb1cd7a4fdc182eb92d024b16c89fa29f
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-06 23:54:01 -0500

    Daily JavaScript update

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -890,13 +890,39 @@
 //     Use a for loop to push the values 1 through 5 onto myArray.
 
 //    PROBLEM:
+// // Setup
+//     const myArray = [];
+//
+// // Only change code below this line
+// for (let i = 1; i <=5; i++) {
+//     myArray.push(i);
+// }
+// console.log(myArray);
+//
+// 06May2022
+// Iterate Odd Numbers With a For Loop
+// For loops don't have to iterate one at a time. By changing our final-expression, we can count by even numbers.
+//
+// We'll start at i = 0 and loop while i < 10. We'll increment i by 2 each loop with i += 2.
+//
+//     const ourArray = [];
+//
+// for (let i = 0; i < 10; i += 2) {
+//     ourArray.push(i);
+// }
+// ourArray will now contain [0, 2, 4, 6, 8]. Let's change our initialization so we can count by odd numbers.
+//
+// Push the odd numbers from 1 through 9 to myArray using a for loop.
+
+
+        //Problem:
 // Setup
-    const myArray = [];
+const myArray = [];
+
 
 // Only change code below this line
-for (let i = 1; i <=5; i++) {
-    myArray.push(i);
-}
+for (let i = 1; i <= 9; i +=2 )
+myArray.push(i);
 console.log(myArray);
 
 
