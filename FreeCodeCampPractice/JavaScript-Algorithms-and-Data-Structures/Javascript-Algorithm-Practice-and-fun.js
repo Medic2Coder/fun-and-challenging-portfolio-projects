e9a06923ae04db70b01d5e1fee08f478b967374c
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-08 14:55:30 -0500

    Daily JavaScript update and Happy Mothers Day to all the unsung heros of our lives

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -782,7 +782,6 @@
 // console.log(secondTree);
 
 
-
 // Record Collection
 // You are given an object literal representing a part of your musical album collection. Each album has a unique id number as its key and several other properties. Not all albums have complete information.
 //
@@ -943,19 +942,41 @@
 //
 // Push the odd numbers from 9 through 1 to myArray using a for loop.
 
-                 //Problem:
+//Problem:
+// // Setup
+// const myArray = [];
+//
+// // Only change code below this line
+// for (let i = 9; i >0; i -=2) {
+//     myArray.push(i);
+// }
+// console.log(myArray)
+// //
+// //
+//
+// //08May2022
+// Iterate Through an Array with a For Loop
+// A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. This code will output each element of the array arr to the console:
+//
+//     const arr = [10, 9, 8, 7, 6];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// Remember that arrays have zero-based indexing, which means the last index of the array is length - 1. Our condition for this loop is i < arr.length, which stops the loop when i is equal to length. In this case the last iteration is i === 4 i.e. when i becomes equal to arr.length - 1 and outputs 6 to the console. Then i increases to 5, and the loop terminates because i < arr.length is false.
+//
+//     Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
 // Setup
-const myArray = [];
+const myArr = [2, 3, 4, 5, 6];
+
 
 // Only change code below this line
-for (let i = 9; i >0; i -=2) {
-    myArray.push(i);
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+    console.log(total);
+
 }
-console.log(myArray)
-//
-//
-
-
 
 
 
