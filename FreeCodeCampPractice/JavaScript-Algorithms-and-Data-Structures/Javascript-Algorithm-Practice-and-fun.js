edde8a7a1359e00f350a3512914e71b43c03b0a4
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-09 17:55:42 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -967,16 +967,51 @@
 //
 //     Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
 // Setup
-const myArr = [2, 3, 4, 5, 6];
+// const myArr = [2, 3, 4, 5, 6];
+//
+//
+// // Only change code below this line
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//     total += myArr[i];
+//     console.log(total);
+//
+// }
 
+//09May2022
+// Nesting For Loops
+// If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. Here is an example:
+//
+//     const arr = [
+//         [1, 2], [3, 4], [5, 6]
+//     ];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+// This outputs each sub-element in arr one at a time. Note that for the inner loop, we are checking the .length of arr[i], since arr[i] is itself an array.
+//
+//     Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+//
+         //Problem:
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i <arr.length; i++) {
+        for (let j = 0; j <arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
 
-// Only change code below this line
-let total = 0;
-for (let i = 0; i < myArr.length; i++) {
-    total += myArr[i];
-    console.log(total);
-
+    // Only change code above this line
+    return product;
 }
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+    
 
 
 
