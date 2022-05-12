917d05a6cf9db18423fee03005b66548424de341
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-12 17:19:19 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -1085,20 +1085,85 @@
 //     Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
 //
 //Problem:
-function sum(arr, n) {
+// function sum(arr, n) {
+//     // Only change code below this line
+//           if (n <= 0) {
+//               return 0;
+//           } else {
+//               return sum(arr, n-1) + arr[n-1];
+//           }
+//     // Only change code above this line
+// }
+//
+// console.log(sum([1, 2, 5], 1));
+
+//12May2022
+// Profile Lookup
+// We have an array of objects representing different people in our contacts lists.
+//
+//     A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+//
+//                                                                                                              The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+//
+// If both are true, then return the "value" of that property.
+//
+//     If name does not correspond to any contacts then return the string No such contact.
+//
+//     If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+//
+       //Problem:
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
     // Only change code below this line
-          if (n <= 0) {
-              return 0;
-          } else {
-              return sum(arr, n-1) + arr[n-1];
-          }
+      //iterate through the contacts array
+    // for (let i = 0; i < contacts.length; i ++) {
+    //     if(contacts[i].firstName === name) {
+    //         if(contacts[i].hasOwnProperty(prop)) {
+    //             return contacts[i][prop];
+    //         } else {
+    //             return "No such property";
+    //
+    //         }
+    //     }
+    // }
+    for (let i = 0; i < contacts.length; i ++) {
+        if(contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
+    return "No such contact";
     // Only change code above this line
 }
 
-console.log(sum([1, 2, 5], 1));
-
-
-
+lookUpProfile("Akira", "likes");
+console.log(lookUpProfile("Akira", "likes"));
 
 
 
