f49978add5fa1eea50e0a163cffe92acf54f58ff
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-15 15:12:00 -0500

    Daily JavaScript practice problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -1194,16 +1194,35 @@
 // Use another function, Math.floor() to round the number down to its nearest whole number between 0 and 9.
 
     //Problem:
-function randomWholeNum() {
+// function randomWholeNum() {
+//
+//     // Only change code below this line
+//
+//     return Math.floor(Math.random() *10);
+// }
+//
+// console.log(randomWholeNum());
+//15May2022
 
+// Generate Random Whole Numbers within a Range
+// Instead of generating a random whole number between zero and a given number like we did before, we can generate a random whole number that falls within a range of two specific numbers.
+//
+//     To do this, we'll define a minimum number min and a maximum number max.
+//
+// Here's the formula we'll use. Take a moment to read it and try to understand what this code is doing:
+//
+//     Math.floor(Math.random() * (max - min + 1)) + min
+// Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+//
+//Problem:
+function randomRange(myMin, myMax) {
     // Only change code below this line
-
-    return Math.floor(Math.random() *10);
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    
+    // Only change code above this line
 }
 
-console.log(randomWholeNum());
-
-
+console.log(randomRange(5, 15));
 
 
 
