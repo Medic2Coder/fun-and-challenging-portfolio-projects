e1b39a021c9c349d0c2923074953a67872a009a2
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-17 14:21:27 -0500

    Daily JavaScript problem Solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -1234,14 +1234,37 @@
 // Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
 //
 //Problem:
-function convertToInteger
-(str) {
-    return parseInt(str);
-}
+// function convertToInteger
+// (str) {
+//     return parseInt(str);
+// }
+//
+//
+// convertToInteger("0056");
+// console.log(convertToInteger("0056"));
 
+//17May2022
+// Use the parseInt Function with a Radix
+// The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+//
+// The function call looks like:
+//
+//     parseInt(string, radix);
+// And here's an example:
+//
+// const a = parseInt("11", 2);
+// The radix variable says that 11 is in the binary system, or base 2. This example converts the string 11 to an integer 3.
+//
+// Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
 
-convertToInteger("0056");
-console.log(convertToInteger("0056"));
+    //Problem:
+    function convertToInteger
+    (str) {
+        return parseInt(str, 2);
+    }
+
+console.log(convertToInteger("10011"));
+    
 
 
 
