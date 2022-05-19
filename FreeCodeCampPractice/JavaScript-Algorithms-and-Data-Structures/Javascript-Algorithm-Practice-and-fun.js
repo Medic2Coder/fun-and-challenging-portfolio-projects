2257434d627005548f8ddb08ae1f7b485e2fd1c6
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-19 16:47:06 -0500

    completion of JavaScript Algorithms and Data Structures on FreeCodeCamp

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Javascript-Algorithm-Practice-and-fun.js
@@ -1463,8 +1463,69 @@
 // }
 
 
+// Mutate an Array Declared with const
+//     If you are unfamiliar with const, check out this challenge.
+//
+//     The const declaration has many use cases in modern JavaScript.
+//
+//     Some developers prefer to assign all their variables using const by default, unless they know they will need to reassign the value. Only in that case, they use let.
+//
+// However, it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration only prevents reassignment of the variable identifier.
+//
+//     const s = [5, 6, 7];
+// s = [1, 2, 3];
+// s[2] = 45;
+// console.log(s);
+// s = [1, 2, 3] will result in an error. The console.log will display the value [5, 6, 45].
+//
+//     As you can see, you can mutate the object [5, 6, 7] itself and the variable s will still point to the altered array [5, 6, 45]. Like all arrays, the array elements in s are mutable, but because const was used, you cannot use the variable identifier s to point to a different array using the assignment operator.
+//
+//     An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
 
+// const s = [5, 7, 2];
+// function editInPlace() {
+//     "use strict";
+//     // Only change code below this line
+//
+//     s[0] = 2;
+//     s[1] = 5;
+//     s[2] = 7;
+//     return s;
+//
+//     // Using s = [2, 5, 7] would be invalid
+//
+//     // Only change code above this line
+// }
+// editInPlace();
+// console.log(s);
 
+//
+// Comparison with the Inequality Operator
+// The inequality operator (!=) is the opposite of the equality operator. It means not equal and returns false where equality would return true and vice versa. Like the equality operator, the inequality operator will convert data types of values while comparing.
+//
+//     Examples
+//
+//     1 !=  2    // true
+// 1 != "1"   // false
+// 1 != '1'   // false
+// 1 != true  // false
+// 0 != false // false
+// Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.
+//
+
+//Problem:
+// Setup
+function testNotEqual(val) {
+    if (val != 99) {
+        return "Not Equal";
+
+    }   else {
+        return "Equal";
+
+    }
+}
+
+console.log(testNotEqual(10));
 
 
 
