fe0e2ad2705cc643c10cb633e2f3c580e3756bf0
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-27 20:02:09 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -265,11 +265,54 @@ console.log(magic());
 //     const spreaded = ...arr;
 // Copy all contents of arr1 into another array arr2 using the spread operator.
 //
-const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-// Copy all contents of arr1 into another array arr2 using the spread operator.
-const arr2 = [...arr1];
-console.log(arr2);
+// const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+// // Copy all contents of arr1 into another array arr2 using the spread operator.
+// const arr2 = [...arr1];
+// console.log(arr2);
 
+
+//27May2022
+// JavaScript Algorithms and Data Structures
+// ES6
+// Use Destructuring Assignment to Extract Values from Objects
+// Destructuring assignment is special syntax introduced in ES6, for neatly assigning values taken directly from an object.
+//
+//     Consider the following ES5 code:
+//
+//     const user = { name: 'John Doe', age: 34 };
+//
+// const name = user.name;
+// const age = user.age;
+// name would have a value of the string John Doe, and age would have the number 34.
+//
+// Here's an equivalent assignment statement using the ES6 destructuring syntax:
+//
+// const { name, age } = user;
+// Again, name would have a value of the string John Doe, and age would have the number 34.
+//
+// Here, the name and age variables will be created and assigned the values of their respective values from the user object. You can see how much cleaner this is.
+//
+//     You can extract as many or few values from the object as you want.
+//
+
+//     Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+     //Problem:
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+//
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+console.log(today);
+console.log(tomorrow);
+
+// Only change code above this line
 
 
 
