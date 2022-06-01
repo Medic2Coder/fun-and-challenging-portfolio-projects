327a7009f4b3c25da0ecbdd781958eb44373557c
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-31 23:24:23 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -296,7 +296,7 @@ console.log(magic());
 //
 
 //     Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
-     //Problem:
+//Problem:
 // const HIGH_TEMPERATURES = {
 //     yesterday: 75,
 //     today: 77,
@@ -396,16 +396,56 @@ console.log(magic());
 //     Variables a and b take the first and second values from the array. After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array. The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array.
 //
 // Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted.
-const source = [1,2,3,4,5,6,7,8,9,10];
-function removeFirstTwo(list) {
-    // Only change code below this line
-    // const [a, b, ...arr] = list; // Change this line
-    const [,, ...arr] = list;
-    // Only change code above this line
-    return arr;
-}
-const arr = removeFirstTwo(source);
-console.log(arr);
-console.log(source);
+// const source = [1,2,3,4,5,6,7,8,9,10];
+// function removeFirstTwo(list) {
+//     // Only change code below this line
+//     // const [a, b, ...arr] = list; // Change this line
+//     const [,, ...arr] = list;
+//     // Only change code above this line
+//     return arr;
+// }
+// const arr = removeFirstTwo(source);
+// console.log(arr);
+// console.log(source);
+
+//31May2022
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// In some cases, you can destructure the object in a function argument itself.
+//
+//     Consider the code below:
+//
+//     const profileUpdate = (profileData) => {
+//         const { name, age, nationality, location } = profileData;
+//
+//     }
+// This effectively destructures the object sent into the function. This can also be done in-place:
+//
+// const profileUpdate = ({ name, age, nationality, location }) => {
+//
+// }
+// When profileData is passed to the above function, the values are destructured from the function parameter for use within the function.
+//
+// Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+//Problem:
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0;
+// Only change code above this line
+console.log(half(stats));
+
+
+
+
+
+
 
 
