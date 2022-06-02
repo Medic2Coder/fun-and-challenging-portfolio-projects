65721d375e49a26263e0bde87e19885ecc973277
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-01 22:44:38 -0500

    JavaScript problem solved using template literals

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -428,19 +428,74 @@ console.log(magic());
 // Use destructuring assignment within the argument to the function half to send only max and min inside the function.
 //Problem:
 
-const stats = {
-    max: 56.78,
-    standard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
-};
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+// };
+//
+// // Only change code below this line
+// const half = ({max, min}) => (max + min) / 2.0;
+// // Only change code above this line
+// console.log(half(stats));
 
-// Only change code below this line
-const half = ({max, min}) => (max + min) / 2.0;
-// Only change code above this line
-console.log(half(stats));
+//01June2022
+// Create Strings using Template Literals
+// A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier.
+//
+//     Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+//
+//     Consider the code below:
+//
+//     const person = {
+//         name: "Zodiac Hasbro",
+//         age: 56
+//     };
+//
+// const greeting = `Hello, my name is ${person.name}!
+// I am ${person.age} years old.`;
+//
+// console.log(greeting);
+// The console will display the strings Hello, my name is Zodiac Hasbro! and I am 56 years old..
+//
+// A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. Similarly, you can include other expressions in your string literal, for example ${a + b}. This new way of creating strings gives you more flexibility to create robust strings.
+//
+// Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+//
+// Use an iterator method (any kind of loop) to get the desired output (shown below).
+//
+// [
+//   '<li class="text-warning">no-var</li>',
+//   '<li class="text-warning">var-on-top</li>',
+//   '<li class="text-warning">linebreak</li>'
+// ]
+
+//Problem:
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    //iterate through element of arr and add a new element consisting composed of a template literal using arr's elementes to create the array failureItems
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    // Only change code above this line
+
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+console.log(failuresList);
+
+
+
 
 
 
