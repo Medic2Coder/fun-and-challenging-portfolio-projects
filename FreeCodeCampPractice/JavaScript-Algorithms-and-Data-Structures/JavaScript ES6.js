a40eb352106b8bd8a54c4d17d57b22571082f532
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-02 20:12:05 -0500

    Daily JavaScript problem solved using syntactic sugar

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -474,27 +474,53 @@ console.log(magic());
 // ]
 
 //Problem:
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-};
-function makeList(arr) {
-    // Only change code below this line
-    const failureItems = [];
-    //iterate through element of arr and add a new element consisting composed of a template literal using arr's elementes to create the array failureItems
-    for (let i = 0; i < arr.length; i++) {
-        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
-    }
+// const result = {
+//     success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//     failure: ["no-var", "var-on-top", "linebreak"],
+//     skipped: ["no-extra-semi", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//     // Only change code below this line
+//     const failureItems = [];
+//     //iterate through element of arr and add a new element consisting composed of a template literal using arr's elementes to create the array failureItems
+//     for (let i = 0; i < arr.length; i++) {
+//         failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+//     }
+//     // Only change code above this line
+//
+//     return failureItems;
+// }
+//
+// const failuresList = makeList(result.failure);
+// console.log(failuresList);
+
+//02June2022
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+// ES6 adds some nice support for easily defining object literals.
+//
+//     Consider the following code:
+//
+//     const getMousePosition = (x, y) => ({
+//         x: x,
+//         y: y
+//     });
+// getMousePosition is a simple function that returns an object containing two properties. ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. Here is the same function from above rewritten to use this new syntax:
+//
+// const getMousePosition = (x, y) => ({ x, y });
+// Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+//
+// Problem:
+const createPerson = (name, age, gender) => {
+    // // Only change code below this line
+    // return {
+    //     name: name,
+    //     age: age,
+    //     gender: gender
+    // };
+    return {name, age, gender};
+
     // Only change code above this line
-
-    return failureItems;
-}
-
-const failuresList = makeList(result.failure);
-console.log(failuresList);
-
-
+};
 
 
 
