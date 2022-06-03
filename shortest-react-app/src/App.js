2df7c82cb81b0ef1195f591e73bbfe461a901e74
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-03 13:31:38 -0500

    set count short react app completed

--- shortest-react-app/src/App.js
+++ shortest-react-app/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
     const myStyle = {
         color: 'red'
     }
+    const [count, setCount] = useState(0);
 
-  return (
-      <div className="app">
-        <h1 style = {myStyle}>Hello There! 🤠
-        </h1>
-      </div>
-  )
+
+    return (
+        <div className="app">
+            <h1 style={myStyle}>Hello There! 🤠
+            </h1>
+            <h2>My Count is { count }</h2>
+            <p>Start editing to see some magic happen :)</p>
+            <button onClick={() => setCount(count + 1)}>Add Count
+            </button>
+        </div>
+    );
 }
+
