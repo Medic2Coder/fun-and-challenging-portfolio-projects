49b7201e482e012d4050ca827b9f6a06324cc8f0
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-03 17:30:32 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -509,19 +509,50 @@ console.log(magic());
 // const getMousePosition = (x, y) => ({ x, y });
 // Use object property shorthand with object literals to create and return an object with name, age and gender properties.
 //
-// Problem:
-const createPerson = (name, age, gender) => {
-    // // Only change code below this line
-    // return {
-    //     name: name,
-    //     age: age,
-    //     gender: gender
-    // };
-    return {name, age, gender};
+// // Problem:
+// const createPerson = (name, age, gender) => {
+//     // // Only change code below this line
+//     // return {
+//     //     name: name,
+//     //     age: age,
+//     //     gender: gender
+//     // };
+//     return {name, age, gender};
+//
+//     // Only change code above this line
+// };
 
-    // Only change code above this line
+//03Jun2022
+// Write Concise Declarative Functions with ES6
+//     When defining functions within objects in ES5, we have to use the keyword function as follows:
+//
+//     const person = {
+//         name: "Taylor",
+//         sayHello: function() {
+//             return `Hello! My name is ${this.name}.`;
+//         }
+//     };
+// With ES6, you can remove the function keyword and colon altogether when defining functions in objects. Here's an example of this syntax:
+//
+// const person = {
+//     name: "Taylor",
+//     sayHello() {
+//         return `Hello! My name is ${this.name}.`;
+//     }
+// };
+// Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
+//
+//Problem:
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear (newGear) {
+        this.gear = newGear;
+    }
 };
-
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
 
 
