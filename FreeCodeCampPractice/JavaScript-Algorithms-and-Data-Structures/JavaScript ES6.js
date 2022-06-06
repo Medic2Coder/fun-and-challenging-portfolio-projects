466a319eb48a6ce6e56c28234e31b81e8b28cc83
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-06 11:03:18 -0500

    Solved daily JavaScript problem using module script

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -644,28 +644,48 @@ console.log(magic());
 
 //Problem:
 // Only change code below this line
-class Thermostat {
-    constructor(fahrenheit) {
-        this.fahrenheit = fahrenheit;
-    }
+// class Thermostat {
+//     constructor(fahrenheit) {
+//         this.fahrenheit = fahrenheit;
+//     }
+//
+//     get temperature() {
+//         return  (5 / 9) * (this.fahrenheit - 32);
+//     }
+//
+//     set temperature(celsius) {
+//         this.fahrenheit = (celsius * 9 )/ 5 + 32;
+//     }
+// }
+//
+// // Only change code above this line
+//
+// const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+// let temp = thermos.temperature; // 24.44 in Celsius
+// thermos.temperature = 26;
+// temp = thermos.temperature; // 26 in Celsius
+//
+// console.log(temp);
 
-    get temperature() {
-        return  (5 / 9) * (this.fahrenheit - 32);
-    }
+             //06Jun2022
+// Create a Module Script
+// JavaScript started with a small role to play on an otherwise mostly HTML web. Today, it’s huge, and some websites are built almost entirely with JavaScript. In order to make JavaScript more modular, clean, and maintainable; ES6 introduced a way to easily share code among JavaScript files. This involves exporting parts of a file for use in one or more other files, and importing the parts you need, where you need them. In order to take advantage of this functionality, you need to create a script in your HTML document with a type of module. Here’s an example:
+//
+//     <script type="module" src="filename.js"></script>
+// A script that uses this module type can now use the import and export features you will learn about in the upcoming challenges.
+//
+//     Add a script to the HTML document of type module and give it the source file of index.js
+//
+//
+//Problem:
+<html>
+<body>
+<!-- Only change code below this line -->
+<script type = "module" src = "index.js"></script>
+<!-- Only change code above this line -->
+</body>
+</html>
 
-    set temperature(celsius) {
-        this.fahrenheit = (celsius * 9 )/ 5 + 32;
-    }
-}
-
-// Only change code above this line
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
-
-console.log(temp);
 
 
 
