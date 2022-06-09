a20505c597b33140aa23689db72eede483cb713e
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-09 18:06:06 -0500

    Daily JavaScript problem solved using star symbol to import

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -725,12 +725,33 @@ console.log(magic());
 //
 // import { add, subtract } from './math_functions.js';
 // Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
-//Problem:
-import {uppercaseString, lowercaseString} from './string_functions.js';
+// //Problem:
+// import {uppercaseString, lowercaseString} from './string_functions.js';
+// // Only change code above this line
+//
+// uppercaseString("hello");
+// lowercaseString("WORLD!");
+
+//09June2022
+// Use * to Import Everything from a File
+// Suppose you have a file and you wish to import all of its contents into the current file. This can be done with the import * as syntax. Here's an example where the contents of a file named math_functions.js are imported into a file in the same directory:
+//
+// import * as myMathModule from "./math_functions.js";
+// The above import statement will create an object called myMathModule. This is just a variable name, you can name it anything. The object will contain all of the exports from math_functions.js in it, so you can access the functions like you would any other object property. Here's how you can use the add and subtract functions that were imported:
+//
+// myMathModule.add(2,3);
+// myMathModule.subtract(5,3);
+// The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
+//
+//     Problem:
+    import * as stringFunctions from './string_functions.js';
 // Only change code above this line
 
-uppercaseString("hello");
-lowercaseString("WORLD!");
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+
+
 
 
 
