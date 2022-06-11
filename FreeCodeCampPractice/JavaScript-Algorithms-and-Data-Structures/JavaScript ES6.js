78f9dbe429782304373a7deb24849a619383c462
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-10 23:40:02 -0500

    Solved Daily JavaScript problem using export default function

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -743,14 +743,37 @@ console.log(magic());
 // myMathModule.subtract(5,3);
 // The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
 //
-//     Problem:
-    import * as stringFunctions from './string_functions.js';
-// Only change code above this line
-
-stringFunctions.uppercaseString("hello");
-stringFunctions.lowercaseString("WORLD!");
-
-
+// //     Problem:
+//     import * as stringFunctions from './string_functions.js';
+// // Only change code above this line
+//
+// stringFunctions.uppercaseString("hello");
+// stringFunctions.lowercaseString("WORLD!");
+//
+// 10June2022
+// Create an Export Fallback with export default
+// In the export lesson, you learned about the syntax referred to as a named export. This allowed you to make multiple functions and variables available for use in other files.
+//
+//     There is another export syntax you need to know, known as export default. Usually you will use this syntax if only one value is being exported from a file. It is also used to create a fallback value for a file or module.
+//
+//     Below are examples using export default:
+//
+// export default function add(x, y) {
+//     return x + y;
+// }
+//
+// export default function(x, y) {
+//     return x + y;
+// }
+// The first is a named function, and the second is an anonymous function.
+//
+// Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. Additionally, you cannot use export default with var, let, or const
+//
+//     The following function should be the fallback value for the module. Please add the necessary code to do so.
+//Problem:
+export default function subtract(x, y) {
+    return x - y;
+}
 
 
 
