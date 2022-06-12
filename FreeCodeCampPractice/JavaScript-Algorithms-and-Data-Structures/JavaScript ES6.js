674f38aa5fa68bdc825035d6e4e1cac6a7c254e0
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-11 20:54:49 -0500

    Daily JavaScript problem solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -770,10 +770,26 @@ console.log(magic());
 // Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. Additionally, you cannot use export default with var, let, or const
 //
 //     The following function should be the fallback value for the module. Please add the necessary code to do so.
-//Problem:
-export default function subtract(x, y) {
-    return x - y;
-}
+// //Problem:
+// export default function subtract(x, y) {
+//     return x - y;
+// }
+
+// 11June2022
+// Import a Default Export
+// In the last challenge, you learned about export default and its uses. To import a default export, you need to use a different import syntax. In the following example, add is the default export of the math_functions.js file. Here is how to import it:
+//
+// import add from "./math_functions.js";
+// The syntax differs in one key place. The imported value, add, is not surrounded by curly braces ({}). add here is simply a variable name for whatever the default export of the math_functions.js file is. You can use any name here when importing a default.
+//
+// In the following code, import the default export from the math_functions.js file, found in the same directory as this file. Give the import the name subtract.
+//
+// //Problem:
+import subtract from './math_functions.js';
+
+// Only change code above this line
+
+subtract(7,4);
 
 
 
