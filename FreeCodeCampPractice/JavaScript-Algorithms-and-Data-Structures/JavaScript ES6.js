f0f48a8acddde9c9b459d48abcee6d5bce8dcc2e
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-12 23:58:01 -0500

    javascript promise solved

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/JavaScript ES6.js
@@ -784,12 +784,26 @@ console.log(magic());
 //
 // In the following code, import the default export from the math_functions.js file, found in the same directory as this file. Give the import the name subtract.
 //
-// //Problem:
-import subtract from './math_functions.js';
+// // //Problem:
+// import subtract from './math_functions.js';
+//
+// // Only change code above this line
+//
+// subtract(7,4);
 
-// Only change code above this line
+//12June2022
+// Create a JavaScript Promise
+// A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, usually asynchronously. When the task completes, you either fulfill your promise or fail to do so. Promise is a constructor function, so you need to use the new keyword to create one. It takes a function, as its argument, with two parameters - resolve and reject. These are methods used to determine the outcome of the promise. The syntax looks like this:
+//
+// const myPromise = new Promise((resolve, reject) => {
+//
+// });
+// Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+const makeServerRequest = new Promise((resolve, reject) => {
 
-subtract(7,4);
+});
+
+
 
 
 
