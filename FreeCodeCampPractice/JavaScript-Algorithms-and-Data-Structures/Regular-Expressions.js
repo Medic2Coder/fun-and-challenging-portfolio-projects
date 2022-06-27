0e4056d2884fb28cc4f1dd3ac90bf7dc7e8b20e7
Michael Stokes <michael.stokes.347@gmail.com>
2022-06-26 22:36:40 -0500

    regex JavaScript probelm solved using  dot match method and case insensitive gi

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
@@ -146,12 +146,41 @@
 
     // Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
 
-    let exampleStr = "Let's have fun with regular expressions!";
-    // let unRegex = /[a-z]un/;      //another example
-    let unRegex = /.un/;
+//     let exampleStr = "Let's have fun with regular expressions!";
+//     // let unRegex = /[a-z]un/;      //another example
+//     let unRegex = /.un/;
+//
+// let result = unRegex.test(exampleStr);
+// console.log(result);
 
-let result = unRegex.test(exampleStr);
+//26June2022
+// Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+//
+//     For example, you want to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u.
+//
+//     let bigStr = "big";
+// let bagStr = "bag";
+// let bugStr = "bug";
+// let bogStr = "bog";
+// let bgRegex = /b[aiu]g/;
+// bigStr.match(bgRegex);
+// bagStr.match(bgRegex);
+// bugStr.match(bgRegex);
+// bogStr.match(bgRegex);
+// In order, the four match calls would return the values ["big"], ["bag"], ["bug"], and null.
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+//
+//     Note: Be sure to match both upper- and lowercase vowels.
+//
+// Problem:
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex);
 console.log(result);
+
+
+
+
 
 
 
