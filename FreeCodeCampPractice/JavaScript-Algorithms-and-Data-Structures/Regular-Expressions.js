332beecbf1c77ba542e95f2dc5d355c3af0d50c8
Michael Stokes <michael.stokes.347@gmail.com>
2022-07-04 23:23:37 -0500

    Daily JavaScript problem solved using Regex negated Character Set

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
@@ -217,11 +217,27 @@
 // jennyStr.match(myRegex);
 // Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
     //Problem:
-    let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[h-s2-6]/gi; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
+//     let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+//
+
+//04July2022
+// Match Single Characters Not Specified
+// So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+//
+//     To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+//
+//     For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+//
+//     Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+//
+//Problem:
+let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aeiou]/gi; // Change this line
+let result = quoteSample.match(myRegex);
 console.log(result);
-    
 
 
 
