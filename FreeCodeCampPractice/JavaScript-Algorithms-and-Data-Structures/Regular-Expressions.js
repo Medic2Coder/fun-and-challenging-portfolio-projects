c49a967565327e468d5e80a4bff7db71cda362b9
Michael Stokes <michael.stokes.347@gmail.com>
2022-07-05 23:48:20 -0500

    Daily JavaScript problem solved using Regex and plus sign

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
@@ -234,10 +234,30 @@
 //     Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
 //
 //Problem:
-let quoteSample = "3 blind mice.";
-let myRegex = /[^0-9aeiou]/gi; // Change this line
-let result = quoteSample.match(myRegex);
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^0-9aeiou]/gi; // Change this line
+// let result = quoteSample.match(myRegex);
+// console.log(result);
+
+//05July2022
+// Match Characters that Occur One or More Times
+// Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
+//
+//     You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+//
+//     For example, /a+/g would find one match in abc and return ["a"]. Because of the +, it would also find a single match in aabc and return ["aa"].
+//
+//     If it were instead checking the string abab, it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no a in the string bcd, it wouldn't find a match.
+//
+// You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses the + sign.
+
+
+//Problem:
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
 console.log(result);
+
 
 
 
