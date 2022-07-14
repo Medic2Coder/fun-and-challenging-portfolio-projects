11c30465f16a07cba12c94dc27cfd762b28d65b3
Michael Stokes <michael.stokes.347@gmail.com>
2022-07-13 23:24:19 -0500

    Daily JavaScript problem solved for RegEx using capital W to find opposite of alphanumerics

--- FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
+++ FreeCodeCampPractice/JavaScript-Algorithms-and-Data-Structures/Regular-Expressions.js
@@ -415,10 +415,32 @@
 //
 
              //Problem:
-    let quoteSample = "The five boxing wizards jump quickly.";
-let alphabetRegexV2 = /\w/g; // Change this line
-let result = quoteSample.match(alphabetRegexV2).length;
+//     let quoteSample = "The five boxing wizards jump quickly.";
+// let alphabetRegexV2 = /\w/g; // Change this line
+// let result = quoteSample.match(alphabetRegexV2).length;
+// console.log(result);
+
+//13July2022
+// Match Everything But Letters and Numbers
+// You've learned that you can use a shortcut to match alphanumerics [A-Za-z0-9_] using \w. A natural pattern you might want to search for is the opposite of alphanumerics.
+//
+// You can search for the opposite of the \w with \W. Note, the opposite pattern uses a capital letter. This shortcut is the same as [^A-Za-z0-9_].
+//
+// let shortHand = /\W/;
+// let numbers = "42%";
+// let sentence = "Coding!";
+// numbers.match(shortHand);
+// sentence.match(shortHand);
+// The first match call would return the value ["%"] and the second would return ["!"].
+//
+//     Use the shorthand character class \W to count the number of non-alphanumeric characters in various quotes and strings.
+
+//Problem:
+let quoteSample = "The five boxing wizards jump quickly.";
+let notAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(notAlphabetRegex).length;
 console.log(result);
+
 
 
 
